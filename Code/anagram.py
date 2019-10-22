5e2aaad9f85ba682d1b9788aed2c83f4237520ec
atleastzero <megan.obryan52@gmail.com>
2019-10-22 13:14:05 -0700

    Updating anagram to return all anagrams instead of a randomized one.

--- Code/anagram.py
+++ Code/anagram.py
@@ -1,18 +1,28 @@
 import random
 
-def anagram(original_string):
-    new_string = ""
-    string_list = list(original_string)
+# uses heap permutation algorithm
+def all_anagrams(original_string, size):
+    all_anagram_list = list()
+    if (size == 1):
+        new_string = ""
+        for letter in original_string:
+            new_string += letter
+        return new_string
+    else:
+        for index in range(size):
+            little_list = all_anagrams(original_string, size - 1)
+            all_anagram_list.append(little_list)
 
-    for index in range(len(string_list)):
-        list_item = random.choice(string_list)
-        new_string += list_item
-        string_list.remove(list_item)
+            if size % 2 == 1:
+                original_string[0], original_string[size - 1] = original_string[size - 1], original_string[0]
+            else:
+                original_string[index], original_string[size - 1] = original_string[size - 1], original_string[index]
 
-    return new_string
+    return all_anagram_list
+    
 
-print(anagram("hello"))
-print(anagram("hello"))
-print(anagram("hello"))
-print(anagram("hello"))
-print(anagram("hello"))
+
+def conv_all_anagrams(original_string):
+    return (all_anagrams(list(original_string), len(original_string)))
+
+print(conv_all_anagrams("ten"))

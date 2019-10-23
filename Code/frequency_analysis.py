48a5643bc949d4ce0d8b2ed151294abc9d8a99f1
atleastzero <megan.obryan52@gmail.com>
2019-10-23 14:32:08 -0700

    Added frequency() function.

--- Code/frequency_analysis.py
+++ Code/frequency_analysis.py
@@ -6,16 +6,16 @@ def histogram(source_text):
     for word in text_list:
         found = False
         for pair in histogram:
-            if pair[0] == word:
+            if pair[0] == word.upper():
                 number = pair[1] + 1
-                histogram.append((word, number))
+                histogram.append((word.upper(), number))
                 histogram.remove(pair)
                 found = True
         if not found:
-            histogram.append((word, 1))
+            histogram.append((word.upper(), 1))
 
     for pair in histogram:
-        if word != "":
+        if pair[0] == "":
             histogram.remove(pair)
 
     return histogram
@@ -23,7 +23,15 @@ def histogram(source_text):
 def unique_words(histogram):
     return len(histogram)
 
+def frequency(word, histogram):
+    for pair in histogram:
+        if pair[0] == word.upper():
+            return pair[1]
+    return 0
+
 string = "To Sherlock Holmes she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for Irene Adler. All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. He was, I take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. He never spoke of the softer passions, save with a gibe and a sneer. They were admirable things for the observer—excellent for drawing the veil from men’s motives and actions. But for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. Grit in a sensitive instrument, or a crack in one of his own high-power lenses, would not be more disturbing than a strong emotion in a nature such as his. And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory."
 
 print(histogram(string))
 print(unique_words(histogram(string)))
+print(frequency("Killer", histogram(string)))
+print(frequency("to", histogram(string)))

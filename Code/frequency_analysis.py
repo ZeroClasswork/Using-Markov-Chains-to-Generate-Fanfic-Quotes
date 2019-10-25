77e07a8fedf9e2e305a533535b14f35271b30833
atleastzero <megan.obryan52@gmail.com>
2019-10-25 12:59:07 -0700

    Added sort function for tuples and list histograms

--- Code/frequency_analysis.py
+++ Code/frequency_analysis.py
@@ -69,10 +69,21 @@ def frequency_dictionary(word, histogram):
     else:
         return 0
 
-def tests():
-    #string = "To Sherlock Holmes she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for Irene Adler. All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. He was, I take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. He never spoke of the softer passions, save with a gibe and a sneer. They were admirable things for the observer—excellent for drawing the veil from men’s motives and actions. But for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. Grit in a sensitive instrument, or a crack in one of his own high-power lenses, would not be more disturbing than a strong emotion in a nature such as his. And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory."
+def sort_tuples_and_lists(histogram):
+    new_histogram = list()
+    for index in range(len(histogram)):
+        lowest_val = histogram[0][0]
+        lowest_pair = histogram[0]
+        for pair in histogram:
+            if pair[0] < lowest_val:
+                lowest_val = pair[0]
+                lowest_pair = pair
+        new_histogram.append(lowest_pair)
+        histogram.remove(lowest_pair)
+    return new_histogram
 
-    string = "to blah to"
+def tests():
+    string = "To Sherlock Holmes she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for Irene Adler. All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. He was, I take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. He never spoke of the softer passions, save with a gibe and a sneer. They were admirable things for the observer—excellent for drawing the veil from men’s motives and actions. But for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. Grit in a sensitive instrument, or a crack in one of his own high-power lenses, would not be more disturbing than a strong emotion in a nature such as his. And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory."
 
     result = tuples_histogram(string)
 
@@ -80,6 +91,7 @@ def tests():
     print(unique_words(result))
     print(frequency_tuples_and_lists("Killer", result))
     print(frequency_tuples_and_lists("to", result))
+    print(sort_tuples_and_lists(result))
 
     result = lists_histogram(string)
 
@@ -87,6 +99,7 @@ def tests():
     print(unique_words(result))
     print(frequency_tuples_and_lists("Killer", result))
     print(frequency_tuples_and_lists("to", result))
+    print(sort_tuples_and_lists(result))
 
     result = dictionary_histogram(string)
 

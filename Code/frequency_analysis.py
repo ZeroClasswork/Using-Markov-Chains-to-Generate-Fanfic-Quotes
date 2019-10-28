a1e6448844322374204a48721bffde56a6e37ae6
atleastzero <megan.obryan52@gmail.com>
2019-10-28 14:43:42 -0700

    [Fix] minor fixes to histograms

--- Code/frequency_analysis.py
+++ Code/frequency_analysis.py
@@ -27,9 +27,7 @@ def lists_histogram(source_text):
         found = False
         for pair in histogram:
             if pair[0] == word.upper() and not found:
-                number = pair[1] + 1
-                histogram.remove(pair)
-                histogram.append([word.upper(), number])
+                pair[1] += + 1
                 found = True
         if not found:
             histogram.append([word.upper(), 1])
@@ -49,8 +47,8 @@ def dictionary_histogram(source_text):
         else:
             histogram[word.upper()] = 1
 
-        if "" in histogram:
-            histogram.pop("")
+    if "" in histogram:
+        histogram.pop("")
 
     return histogram
 
@@ -106,7 +104,7 @@ def frequency_dictionary(word, histogram):
 
 def sort_tuples_and_lists(histogram):
     new_histogram = list()
-    for index in range(len(histogram)):
+    for _ in range(len(histogram)):
         lowest_val = histogram[0][0]
         lowest_pair = histogram[0]
         for pair in histogram:
@@ -118,6 +116,8 @@ def sort_tuples_and_lists(histogram):
     return new_histogram
 
 def tests():
+    #string = "one fish two fish red fish blue fish"
+
     string = "To Sherlock Holmes she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for Irene Adler. All emotions, and that one particularly, were abhorrent to his cold, precise but admirably balanced mind. He was, I take it, the most perfect reasoning and observing machine that the world has seen, but as a lover he would have placed himself in a false position. He never spoke of the softer passions, save with a gibe and a sneer. They were admirable things for the observer—excellent for drawing the veil from men’s motives and actions. But for the trained reasoner to admit such intrusions into his own delicate and finely adjusted temperament was to introduce a distracting factor which might throw a doubt upon all his mental results. Grit in a sensitive instrument, or a crack in one of his own high-power lenses, would not be more disturbing than a strong emotion in a nature such as his. And yet there was but one woman to him, and that woman was the late Irene Adler, of dubious and questionable memory."
 
     result = tuples_histogram(string)

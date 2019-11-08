2eba1c15d6fb776417825225e3c246584d480895
atleastzero <megan.obryan52@gmail.com>
2019-11-07 20:14:38 -0800

    Implemented Dictogram.py

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -19,11 +19,22 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        # Increases word frequency by count
+        self.tokens += count
+        try:
+            current_count = self.__getitem__(word)
+            self.__setitem__(word, current_count + count)
+        except:
+            self.__setitem__(word, count)
+            self.types += 1
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        # Retrieves word frequency count
+        try:
+            return self.__getitem__(word)
+        except:
+            return 0
 
 
 def print_histogram(word_list):

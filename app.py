3b342fba60d4aefd31002c998519329a637fb31c
atleastzero <megan.obryan52@gmail.com>
2019-11-07 19:54:59 -0800

    Updated app.py for recent refactoring.

--- app.py
+++ app.py
@@ -2,28 +2,31 @@ from flask import Flask
 import re
 import random
 import os
-from Code import sample
+import sample
+import cleanup
+from flask import request
+
+import sentence
 
 app = Flask(__name__)
 
-@app.route('/')
-def generate_sentence():
+def get_words():
     f = open("./text/rumpelstiltskin.txt", "r")
     contents = f.read()
-    # words_list = re.split('\W+', contents)
     f.close()
-    new_sentence = ""
-    number = 10
-    for i in range(number):
-        new_sentence += sample.random_word_list(contents).lower()
-        if i == 0:
-            new_sentence = new_sentence.capitalize()
-        if i != number - 1:
-            new_sentence += " "
-        else:
-            punctuation = [".", "!", "?", "...", "!?"]
-            new_sentence += random.choice(punctuation)
-    return new_sentence
+    return contents
     
+@app.route('/', methods=['GET'])
+def generate_sentence():
+    texts = ['rumpelstiltskin.txt', 'tom_thumb.txt']
+    contents = cleanup.clean_file(texts[0])
+    number = request.args.get('num')
+    if number is None:
+        number = 10
+    else:
+        number = int(number)
+    new_sentence = sentence.generate_sentence(number, contents)
+    return new_sentence
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))

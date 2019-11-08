760973e252ae8d898f125f00226a21e637681e10
atleastzero <megan.obryan52@gmail.com>
2019-11-07 22:18:41 -0800

    Updated Markovogram to be able to generate random Markov chains.

--- markovogram.py
+++ markovogram.py
@@ -1,5 +1,7 @@
 from __future__ import division, print_function  # Python 2 and 3 compatibility
-
+import sample
+import random
+import cleanup
 
 class Markovogram(dict):
     """Markovogram is a markov histogram implemented as a subclass of the dict type."""
@@ -42,6 +44,30 @@ class Markovogram(dict):
                     appended_sub_dict[key] = value
                 self[word1] = appended_sub_dict
 
+    def frequency(self, word):
+        histogram = self[word.upper()]
+        frequency = 0
+        for _, value in histogram.items():
+            frequency += value
+        return value
+
+    def sample_next(self, recent_word):
+        try:
+            histogram = self[recent_word.upper()]
+            index = random.randrange(self.frequency(recent_word))
+            value = 0
+            last_word = "ISSUEINLASTWORD"
+            for word, value in histogram.items():
+                last_word = word
+                if value <= index:
+                    value += value
+                else:
+                    return word
+            return last_word
+        except:
+            return "ISSUEINEXCEPT"
+        
+
 def print_histogram(word_list):
     print('word list: {}'.format(word_list))
     # Create a dictogram and display its contents
@@ -49,7 +75,21 @@ def print_histogram(word_list):
     print('markovogram: {}'.format(histogram))
     print('{} tokens, {} types'.format(histogram.tokens, histogram.types))
 
-def main():
+def random_walk(word_list, sentence_length):
+    new_sentence = ""
+    recent_word = sample.random_word_tuple(word_list).capitalize()
+    markovogram = Markovogram(word_list)
+    for i in range(sentence_length - 1):
+        new_sentence += recent_word
+        if i != sentence_length - 2:
+            new_sentence += " "
+        else:
+            punctuation = [".", "!", "?", "...", "!?"]
+            new_sentence += random.choice(punctuation)
+        recent_word = markovogram.sample_next(recent_word).lower()
+    return new_sentence
+
+def tests():
     import sys
     arguments = sys.argv[1:]  # Exclude script name in first argument
     if len(arguments) >= 1:
@@ -67,6 +107,12 @@ def main():
                           ' if a wood chuck could chuck wood')
         print_histogram(woodchuck_text.split())
 
+def main():
+    texts = ['rumpelstiltskin.txt', 'tom_thumb.txt']
+
+    contents = cleanup.clean_file(texts[0])
+    
+    print(random_walk(contents, 10))
 
 if __name__ == '__main__':
     main()

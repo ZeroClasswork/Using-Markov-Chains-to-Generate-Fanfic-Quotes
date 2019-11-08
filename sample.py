231116f59dcca78fe46977e399ce65330c8982ca
atleastzero <megan.obryan52@gmail.com>
2019-11-07 19:54:07 -0800

    Fixed minor issues about list vs. string discrepancy in sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ import cleanup
 def random_word_tuple(input_string):
     input_list = cleanup.clean_text(input_string)
     tuples_histogram = histogram.tuples_histogram(input_list)
-    index = random.randrange(len(input_list.split(" ")))
+    index = random.randrange(len(input_list))
     value = 0
     hist_ind = 0
     while value <= index and hist_ind < len(tuples_histogram):
@@ -18,8 +18,8 @@ def random_word_tuple(input_string):
 
 def random_word_list(input_string):
     input_list = cleanup.clean_text(input_string)
-    list_histogram = histogram.lists_histogram(inp)
-    index = random.randrange(len(input_string.split(" ")))
+    list_histogram = histogram.lists_histogram(input_list)
+    index = random.randrange(len(input_list))
     value = 0
     hist_ind = 0
     while value <= index and hist_ind < len(list_histogram):
@@ -27,7 +27,7 @@ def random_word_list(input_string):
         hist_ind += 1
     return list_histogram[hist_ind - 1][0]
 
-if __name__== "__main__":
+def tests():
     # input_list = sys.argv[1:]
     one = 0
     two = 0
@@ -37,7 +37,7 @@ if __name__== "__main__":
 
     input_string = "one fish two fish red fish blue fish"
 
-    for count in range(100000):
+    for _ in range(100000):
         # word = random_word_tuple(input_string)
         word = random_word_list(input_string)
         if word == "ONE":
@@ -56,3 +56,6 @@ if __name__== "__main__":
     print("red:  " + str(1.0 * red  / 1000) + "%")
     print("blue: " + str(1.0 * blue / 1000) + "%")
     print("fish: " + str(1.0 * fish / 1000) + "%")
+
+# if __name__== "__main__":
+#     tests()
